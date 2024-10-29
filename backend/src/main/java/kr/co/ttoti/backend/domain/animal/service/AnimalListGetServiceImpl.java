e1bac508c9eee52149cefef641ae7bdf93c7f9d3
채이슬 <vnfmslss@naver.com>
2024-10-29 17:25:00 +0900

    :sparkles: feat: 사용가능한 동물 목록 조회 API 구현

---
+++ backend/src/main/java/kr/co/ttoti/backend/domain/animal/service/AnimalListGetServiceImpl.java
@@ -0,0 +1,27 @@
+package kr.co.ttoti.backend.domain.animal.service;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import kr.co.ttoti.backend.domain.animal.AnimalRepository;
+import kr.co.ttoti.backend.domain.animal.dto.AnimalDto;
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class AnimalListGetServiceImpl implements AnimalListGetService{
+
+	private final AnimalRepository animalRepository;
+
+	@Override
+	public List<AnimalDto> getAnimalList(boolean animalIsAvailable) {
+		return animalRepository.findByAnimalIsAvailable(animalIsAvailable).stream()
+			.map(animal -> AnimalDto.builder()
+				.animalName(animal.getAnimalName())
+				.animalDescription(animal.getAnimalDescription())
+				.animalImageUrl(animal.getAnimalImageUrl())
+				.build())
+			.toList();
+	}
+}
