8dacf8aa6db46d6cfa016b83cdefb1dd5e3a2172
채이슬 <vnfmslss@naver.com>
2024-10-31 17:59:29 +0900

    :sparkles: feat: AnimalRepository 어노테이션 추가

--- backend/src/main/java/kr/co/ttoti/backend/domain/animal/AnimalRepository.java
+++ backend/src/main/java/kr/co/ttoti/backend/domain/animal/AnimalRepository.java
@@ -4,9 +4,11 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import kr.co.ttoti.backend.domain.animal.entity.Animal;
 
+@Repository
 public interface AnimalRepository extends JpaRepository<Animal, Integer> {
 
 	List<Animal> findByAnimalIsAvailable(boolean animalIsAvailable);
