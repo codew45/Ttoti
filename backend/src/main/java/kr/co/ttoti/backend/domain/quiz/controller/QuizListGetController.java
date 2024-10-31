022c4400b561878f3d260b6c5339cd3e4339c21e
채이슬 <vnfmslss@naver.com>
2024-10-31 19:21:51 +0900

    :sparkles: feat: 퀴즈 목록 조회 컨트롤러 구현 완료

--- backend/src/main/java/kr/co/ttoti/backend/domain/quiz/controller/QuizListGetController.java
+++ backend/src/main/java/kr/co/ttoti/backend/domain/quiz/controller/QuizListGetController.java
@@ -1,5 +1,6 @@
 package kr.co.ttoti.backend.domain.quiz.controller;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -7,8 +8,10 @@ import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import kr.co.ttoti.backend.domain.quiz.dto.QuizListGetDto;
 import kr.co.ttoti.backend.domain.quiz.service.QuizListGetService;
 import kr.co.ttoti.backend.global.dto.ResponseDto;
+import kr.co.ttoti.backend.global.status.SuccessCode;
 import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
@@ -22,7 +25,13 @@ public class QuizListGetController {
 	ResponseEntity<ResponseDto<?>> getQuizList(@RequestHeader Integer memberId,
 		@PathVariable("ttoti-id") Integer ttotiId) {
 
-		quizListGetService.getQuizList(memberId, ttotiId);
-		return null;
+		QuizListGetDto result = quizListGetService.getQuizList(memberId, ttotiId);
+
+		if(result == null){
+			return ResponseEntity.ok(ResponseDto.success(SuccessCode.QUIZ_LIST_GET_SUCCESS_NO_CONTENT));
+		}
+
+		return ResponseEntity.ok(ResponseDto.success(
+			SuccessCode.QUIZ_LIST_GET_SUCCESS, result));
 	}
 }
