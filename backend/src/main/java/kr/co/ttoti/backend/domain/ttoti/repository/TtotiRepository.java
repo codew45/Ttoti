1ab9b005bc5323c5e9d42fa1829af2b6fcfaf1c7
채이슬 <vnfmslss@naver.com>
2024-10-31 17:59:21 +0900

    :sparkles: feat: TtotiRepository 어노테이션 추가

--- backend/src/main/java/kr/co/ttoti/backend/domain/ttoti/repository/TtotiRepository.java
+++ backend/src/main/java/kr/co/ttoti/backend/domain/ttoti/repository/TtotiRepository.java
@@ -1,8 +1,10 @@
 package kr.co.ttoti.backend.domain.ttoti.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import kr.co.ttoti.backend.domain.ttoti.entity.Ttoti;
 
+@Repository
 public interface TtotiRepository extends JpaRepository<Ttoti, Integer> {
 }
