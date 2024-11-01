ba2cbbc2fa2fbc2a0f5d2927a646119faab24043
김호진 <ghwls1025@naver.com>
2024-11-01 14:58:00 +0900

    Fe/feature/room waiting page/s11 p31 a110 220

--- frontend/ttoti/src/components/common/ProfileComponents.tsx
+++ frontend/ttoti/src/components/common/ProfileComponents.tsx
@@ -1,31 +1,46 @@
 import styled from 'styled-components';
 import { SizeProps } from 'src/types/CircleSize';
+import { ReadyProps } from 'src/types/ReadyState';
+import readyIcon from '@assets/profiles/readyIcon.png';
 
 const ProfileCircle = styled.div<SizeProps>`
   display: flex;
   justify-content: center;
   align-items: center;
+  position: relative;
   width: ${({ size }) => size};
   height: ${({ size }) => size};
-  background-color: white;
+  background-color: ${({ $ready }) => $ready === true ? '#67c431' : 'white' };
   border-radius: 50%;
 `
 
-const ProfileImage = styled.img`
+const ProfileImage = styled.img<ReadyProps>`
   width: 92%;
   height: 92%;
   border-radius: 50%;
+  filter: brightness(${({ $ready }) => $ready === true ? '50%' : '100%' });
+`;
+
+const ReadyIcon = styled.img`
+  position: absolute;
+  /* top: 50%; */
+  /* left: 50%; */
+  width: 57%;
+  height: 41%;
+  /* transform: translate(-50%, -50%); */
 `;
 
 interface ProfileContainerProps {
   src: string;
   size: string;
+  ready: boolean;
 }
 
-const ProfileContainer: React.FC<ProfileContainerProps> = ({ src, size }) => {
+const ProfileContainer: React.FC<ProfileContainerProps> = ({ src, size, ready }) => {
   return (
-    <ProfileCircle size={size}>
-      <ProfileImage src={src} alt='Profile' />
+    <ProfileCircle size={size} $ready={ready}>
+      <ProfileImage src={src} alt='Profile' $ready={ready} />
+      {ready && <ReadyIcon src={readyIcon} alt="Ready Icon" />}
     </ProfileCircle>
   );
 };
