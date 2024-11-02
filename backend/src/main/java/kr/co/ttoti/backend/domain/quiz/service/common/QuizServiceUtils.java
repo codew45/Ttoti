c302444909c5dca2c464891ba76538e9f7b3d858
채이슬 <vnfmslss@naver.com>
2024-11-02 18:56:21 +0900

    :sparkles: feat: 퀴즈 내용, 선택지, 응답 여부 등을 담고 있는 QuizHistoryDto 만드는 mapToQuizHistoryDto 추가

---
+++ backend/src/main/java/kr/co/ttoti/backend/domain/quiz/service/common/QuizServiceUtils.java
@@ -0,0 +1,39 @@
+package kr.co.ttoti.backend.domain.quiz.service.common;
+
+import java.util.Map;
+import java.util.stream.Collectors;
+
+import org.springframework.stereotype.Component;
+
+import kr.co.ttoti.backend.domain.quiz.dto.QuizHistoryDto;
+import kr.co.ttoti.backend.domain.quiz.entity.QuizAnswer;
+import kr.co.ttoti.backend.domain.quiz.entity.QuizChoice;
+import kr.co.ttoti.backend.domain.quiz.repository.QuizChoiceRepository;
+import lombok.RequiredArgsConstructor;
+
+@Component
+@RequiredArgsConstructor
+public class QuizServiceUtils {
+
+	private final QuizChoiceRepository quizChoiceRepository;
+
+	public QuizHistoryDto mapToQuizHistoryDto(QuizAnswer quizAnswer) {
+		Map<Integer, String> quizChoiceMap = quizChoiceRepository.findByQuizId(
+			quizAnswer.getQuiz().getQuizId()).stream().collect(
+			Collectors.toMap(
+				QuizChoice::getQuizChoiceNumber,
+				QuizChoice::getQuizChoiceContent));
+
+		return QuizHistoryDto.builder()
+			.quizDate(quizAnswer.getQuizDate())
+			.quizChoiceMap(quizChoiceMap)
+			.quizChoiceContent(quizAnswer.getQuiz().getQuizContent())
+			.quizType(quizAnswer.getQuiz().getQuizType().toString())
+			.isManittoAnswered(quizAnswer.getIsManittoAnswered())
+			.manittoAnswer(quizAnswer.getManittoAnswer())
+			.isManitiAnswered(quizAnswer.getIsManitiAnswered())
+			.manitiAnswer(quizAnswer.getManitiAnswer())
+			.quizAnswerIsCorrect(quizAnswer.getQuizAnswerIsCorrect())
+			.build();
+	}
+}
