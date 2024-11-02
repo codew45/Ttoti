1725678f1487eabcbb96e4813f33fc7f4fb511ae
채이슬 <vnfmslss@naver.com>
2024-11-02 19:47:50 +0900

    :sparkles: feat: 퀴즈 목록 요청 응답 Dto 형태 수정

---
+++ backend/src/main/java/kr/co/ttoti/backend/domain/quiz/dto/QuizListGetResponse.java
@@ -0,0 +1,19 @@
+package kr.co.ttoti.backend.domain.quiz.dto;
+
+import java.util.List;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.ToString;
+
+@ToString
+@Getter
+@Builder
+public class QuizListGetResponse {
+
+	List<QuizHistoryDto> manittoQuizList;
+	List<QuizHistoryDto> manitiQuizList;
+
+	QuizHistoryDto todayManittoQuiz;
+	QuizHistoryDto todayManitiQuiz;
+}
