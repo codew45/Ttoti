0f6c0bf2cc78458e7cccd72743d467a6bf97d5f7
채이슬 <vnfmslss@naver.com>
2024-11-12 15:08:08 +0900

    :recycle: refactor: FCM 알림 보내는 것 global 패키지로 이동

---
+++ backend/src/main/java/kr/co/ttoti/backend/global/fcm/controller/FCMController.java
@@ -0,0 +1,32 @@
+package kr.co.ttoti.backend.global.fcm.controller;
+
+import java.util.concurrent.ExecutionException;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import kr.co.ttoti.backend.global.auth.annotation.MemberId;
+import kr.co.ttoti.backend.global.fcm.dto.FCMDeviceTokenCreateRequest;
+import kr.co.ttoti.backend.global.fcm.service.FCMSendService;
+import kr.co.ttoti.backend.global.util.RedisUtil;
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/ttoti/fcm")
+public class FCMController {
+
+	private final RedisUtil redisUtil;
+	private final FCMSendService fcmSendService;
+
+	@PostMapping("/device-token")
+	public ResponseEntity<Void> saveFCMToken(@RequestBody FCMDeviceTokenCreateRequest fcmDeviceTokenCreateRequest,
+		@MemberId Integer memberId) throws ExecutionException, InterruptedException {
+		redisUtil.setDeviceToken(fcmDeviceTokenCreateRequest, memberId);
+		System.out.println(fcmDeviceTokenCreateRequest.getDeviceToken());
+		return ResponseEntity.ok(null);
+	}
+}
