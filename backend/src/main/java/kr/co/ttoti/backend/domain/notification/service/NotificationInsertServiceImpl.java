4b63de81b0899ee659d076504c5937c99d2113f8
채이슬 <vnfmslss@naver.com>
2024-11-12 11:08:05 +0900

    :sparkles: feat: 알림함 추가 로직 전체적으로 점검

--- backend/src/main/java/kr/co/ttoti/backend/domain/notification/service/NotificationInsertServiceImpl.java
+++ backend/src/main/java/kr/co/ttoti/backend/domain/notification/service/NotificationInsertServiceImpl.java
@@ -1,10 +1,14 @@
 package kr.co.ttoti.backend.domain.notification.service;
 
+import java.util.List;
+
 import org.springframework.stereotype.Service;
 
 import kr.co.ttoti.backend.domain.notification.entity.Notification;
 import kr.co.ttoti.backend.domain.notification.entity.NotificationType;
 import kr.co.ttoti.backend.domain.notification.repository.NotificationRepository;
+import kr.co.ttoti.backend.domain.room.entity.Room;
+import kr.co.ttoti.backend.domain.room.repository.RoomMemberRepository;
 import lombok.RequiredArgsConstructor;
 
 @Service
@@ -12,6 +16,7 @@ import lombok.RequiredArgsConstructor;
 public class NotificationInsertServiceImpl implements NotificationInsertService {
 
 	private final NotificationRepository notificationRepository;
+	private final RoomMemberRepository roomMemberRepository;
 
 	public void insertNotificationToMemberInRoom(Integer memberId, Integer roomId, NotificationType notificationType) {
 		notificationRepository.save(Notification.builder()
@@ -21,5 +26,13 @@ public class NotificationInsertServiceImpl implements NotificationInsertService 
 			.notificationReason(notificationType.getReason())
 			.notificationIsRead(false)
 			.build());
+	}
+
+	public void insertNotificationToAllMembersInRoom(Room room, NotificationType notificationType) {
+		List<Integer> roomMemberIdList = roomMemberRepository.findMemberIdsByRoomAndIsDeletedFalse(room);
+		Integer roomId = room.getRoomId();
+		roomMemberIdList.forEach(
+			roomMemberId -> insertNotificationToMemberInRoom(roomMemberId, roomId,
+				notificationType));
 	}
 }
--- backend/src/main/java/kr/co/ttoti/backend/domain/room/repository/RoomMemberRepository.java
+++ backend/src/main/java/kr/co/ttoti/backend/domain/room/repository/RoomMemberRepository.java
@@ -8,9 +8,9 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
-import kr.co.ttoti.backend.global.auth.entity.Member;
 import kr.co.ttoti.backend.domain.room.entity.Room;
 import kr.co.ttoti.backend.domain.room.entity.RoomMember;
+import kr.co.ttoti.backend.global.auth.entity.Member;
 
 @Repository
 public interface RoomMemberRepository extends JpaRepository<RoomMember, Integer> {
@@ -23,24 +23,32 @@ public interface RoomMemberRepository extends JpaRepository<RoomMember, Integer>
 
 	Optional<RoomMember> findByMemberAndRoom(Member member, Room room);
 
-	Optional<RoomMember> findByRoomAndMemberAndRoomMemberIsDeleted(Room room, Member member, Boolean roomMemberIsDeleted);
+	Optional<RoomMember> findByRoomAndMemberAndRoomMemberIsDeleted(Room room, Member member,
+		Boolean roomMemberIsDeleted);
 
 	List<RoomMember> findByRoomAndRoomMemberIsDeletedAndRoomMemberIsReady(Room room, Boolean roomMemberIsDeleted,
 		Boolean roomMemberIsReady);
 
 	@Query(value = "SELECT m.memberName " +
-			"FROM RoomMember rm " +
-			"JOIN rm.member m " +
-			"WHERE rm.room.roomId = :roomId")
-    List<String> getNamesByRoomId(@Param("roomId") Integer roomId);
+		"FROM RoomMember rm " +
+		"JOIN rm.member m " +
+		"WHERE rm.room.roomId = :roomId")
+	List<String> getNamesByRoomId(@Param("roomId") Integer roomId);
 
 	@Query("SELECT rm2.member " +
-			"FROM RoomMember rm " +
-			"JOIN rm.room r " +
-			"JOIN RoomMember rm2 ON rm2.room = r " +
-			"WHERE rm.member.memberId = :memberId " +
-			"AND rm2.member.memberId != :memberId " +
-			"AND r.roomIsDeleted = false " +
-			"AND r.roomIsFinished = true ")
+		"FROM RoomMember rm " +
+		"JOIN rm.room r " +
+		"JOIN RoomMember rm2 ON rm2.room = r " +
+		"WHERE rm.member.memberId = :memberId " +
+		"AND rm2.member.memberId != :memberId " +
+		"AND r.roomIsDeleted = false " +
+		"AND r.roomIsFinished = true ")
 	List<Member> getFriendsByMemberId(Integer memberId);
+
+	@Query("SELECT rm.member.memberId " +
+		"FROM RoomMember rm " +
+		"WHERE rm.room = :room " +
+		"AND rm.roomMemberIsDeleted = false")
+	List<Integer> findMemberIdsByRoomAndIsDeletedFalse(@Param("room") Room room);
+
 }
