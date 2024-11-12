82e3440c8a6bcbfb53eac8d71611615775e25eb2
채이슬 <vnfmslss@naver.com>
2024-11-12 15:09:26 +0900

    :sparkles: feat: fcm 링크 이동 테스트

--- backend/src/main/java/kr/co/ttoti/backend/global/fcm/controller/FCMController.java
+++ backend/src/main/java/kr/co/ttoti/backend/global/fcm/controller/FCMController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import kr.co.ttoti.backend.domain.notification.entity.NotificationType;
 import kr.co.ttoti.backend.global.auth.annotation.MemberId;
 import kr.co.ttoti.backend.global.fcm.dto.FCMDeviceTokenCreateRequest;
 import kr.co.ttoti.backend.global.fcm.service.FCMSendService;
@@ -27,6 +28,8 @@ public class FCMController {
 		@MemberId Integer memberId) throws ExecutionException, InterruptedException {
 		redisUtil.setDeviceToken(fcmDeviceTokenCreateRequest, memberId);
 		System.out.println(fcmDeviceTokenCreateRequest.getDeviceToken());
+		// 링크 이동 되나 테스트
+		fcmSendService.sendToFCM(memberId, NotificationType.FINAL_GUESS_ANSWER_REMINDER);
 		return ResponseEntity.ok(null);
 	}
 }
